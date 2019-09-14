ffd73d0dc0d66978342d42361c643b05a5112188
dgebert <dgebert91@gmail.com>
2019-09-14 12:41:45 +0100

    Initial commit

---
+++ src/main/java/com/gebert/quizant/QuizantApplication.java
@@ -0,0 +1,13 @@
+package com.gebert.quizant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizantApplication.class, args);
+	}
+
+}
